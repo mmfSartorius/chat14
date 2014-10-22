8047b89b344eea29b981101253c774124d8f0b05
mmfSartorius <mmf.sartorius@gmail.com>
2014-10-22 18:00:18 +0300

    Логика ответа

--- chat14/src/com/chat14/GcmAckIntentService.java
+++ chat14/src/com/chat14/GcmAckIntentService.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.chat14.helpers.CompressUtils;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmAckIntentService extends IntentService {
@@ -39,27 +40,60 @@ public class GcmAckIntentService extends IntentService {
 
 			} else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
 					.equals(messageType)) {
-				if (extras.getBoolean("c")) {
-
-				} else {
-					String msg = null;
-					String messageFromBundle = extras.getString("p");
-					try {
-						JSONObject json = new JSONObject(messageFromBundle);
-						msg = json.getString("msg");
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						msg = e.toString();
-					}
-					Log.d(TAG, msg);
-
-				}
+				String msg = getMessageFromBundle(extras);
 				Log.i(TAG, extras.toString());
 
 			}
 		}
 		GcmBroadcastReceiver.completeWakefulIntent(intent);
+	}
+
+	private String getMessageFromBundle(Bundle bundle) {
+		JSONObject json;
+		String messageFromBundle = bundle.getString("p");
+		String msg = null;
+
+		if (Boolean.valueOf(bundle.getString("c"))) {
+
+			switch (Integer.parseInt(bundle.getString("t"))) {
+			case 0:
+				msg = "Successful registration";
+				break;
+
+			case 1:
+				msg = "Successful entry";
+				break;
+			case 2:
+				int decompressedLenght = Integer
+						.parseInt(bundle.getString("s"));
+				String decompressedMessage = CompressUtils
+						.getDecompressedMessage(messageFromBundle,
+								decompressedLenght);
+				try {
+					json = new JSONObject(decompressedMessage);
+					msg = json.getString("msg");
+				} catch (JSONException e) {
+					e.printStackTrace();
+					msg = e.toString();
+				}
+				break;
+			default:
+				msg = "Error";
+				break;
+			}
+
+		} else {
+			try {
+				json = new JSONObject(messageFromBundle);
+				msg = json.getString("msg");
+			} catch (JSONException e) {
+				e.printStackTrace();
+				msg = e.toString();
+			}
+			Log.d(TAG, msg);
+
+		}
+		return msg;
 	}
 
 }
